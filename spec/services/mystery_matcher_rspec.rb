546f1f48035e08488496c5dd3ee11fead7f1bae9
tsogbadrakh <tsogbadrakh@globalsign.com>
2022-01-15 21:32:51 +0900

    Add test with constant data

--- spec/services/mystery_matcher_rspec.rb
+++ spec/services/mystery_matcher_rspec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MysteryMatcher do
-  let!(:users) { create_list(:user, 10) }
+  let!(:users) { create_list(:user, 8) }
   let(:described_object) { described_class.new(users) }
 
   describe 'init_connection_graph' do
@@ -21,15 +21,15 @@ RSpec.describe MysteryMatcher do
   end
 
   describe 'find_mystery_pairs' do
+    subject do
+      described_object.find_mystery_pairs
+    end
+
+    before do
+      described_object.init_connection_graph
+    end
+
     context 'when there is no old data pairs exist' do
-      subject do
-        described_object.find_mystery_pairs
-      end
-
-      before do
-        described_object.init_connection_graph
-      end
-
       it 'should return array of pair of users' do
         pairs = subject
         expect(pairs).to be_a(Array)
@@ -37,14 +37,37 @@ RSpec.describe MysteryMatcher do
         expect(pairs.flatten.pluck(:id).compact.present?).to be true
       end
 
-      it 'should not create duplicated pairs' do
+      it 'should not create duplicated pairs with different deparment' do
         pairs = subject
         pairs.each_with_object({}) do |pair, acc|
           expect(acc[pair[0][:id]].nil?).to be true
           expect(acc[pair[1][:id]].nil?).to be true
+          expect(pair[0][:department] != pair[1][:department]).to be true
           acc[pair[0][:id]] = pair[0][:department]
           acc[pair[1][:id]] = pair[1][:department]
         end
+      end
+    end
+
+    context 'when constant userlist' do
+      let!(:users) do
+        [create(:user, id: 1, department: 'operations'),
+         create(:user, id: 2, department: 'operations'),
+         create(:user, id: 3, department: 'risk'),
+         create(:user, id: 4, department: 'management'),
+         create(:user, id: 5, department: 'development and data'),
+         create(:user, id: 6, department: 'marketing'),
+         create(:user, id: 7, department: 'development and data'),
+         create(:user, id: 8, department: 'development and data')]
+      end
+
+      it 'should return expected result' do
+        expect(subject).to eq([
+                                [{ 'id' => 5, 'department' => 'development and data' }, { 'id' => 1, 'department' => 'operations' }],
+                                [{ 'id' => 7, 'department' => 'development and data' }, { 'id' => 2, 'department' => 'operations' }],
+                                [{ 'id' => 8, 'department' => 'development and data' }, { 'id' => 3, 'department' => 'risk' }],
+                                [{ 'id' => 4, 'department' => 'management' }, { 'id' => 6, 'department' => 'marketing' }]
+                              ])
       end
     end
   end
