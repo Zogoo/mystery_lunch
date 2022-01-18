424e4e5bd0d380b2ee194b076b09412b53baf6f1
tsogbadrakh <tsogbadrakh@globalsign.com>
2022-01-19 00:39:13 +0900

    User panel improvement

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -19,4 +19,8 @@ module UsersHelper
   def current_user_edit_url
     link_to('Edit', edit_user_path(user)) if current_user.id.to_s == params[:id]
   end
+
+  def admin_panel_url
+    link_to('Admin panel', admin_dashboard_index_path) if current_user.admin?
+  end
 end
