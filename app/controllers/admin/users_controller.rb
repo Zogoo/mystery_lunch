ff0d09923f7f42c276a2bde9b73a5a74f52b9dcb
tsogbadrakh <tsogbadrakh@globalsign.com>
2022-02-06 13:35:01 +0900

    admin/user controller test

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -29,7 +29,7 @@ class Admin::UsersController < Admin::BaseController
     respond_to do |format|
       if @user.save
         @user.add_for_future_pair!
-        format.html { redirect_to admin_users_url(@user), notice: 'User was successfully created.' }
+        format.html { redirect_to admin_users_url, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin::Dashboards', type: :request do
+  let!(:user) { create(:user, password: '!QAZ2wsx') }
+
+  before do
+    user.admin!
+    sign_in user
+  end
+
   describe 'GET /admin/dashboard/index' do
     it 'returns http success' do
       get admin_dashboard_index_url
