90f018dad2afb9d4c12a3c38cd6ae8331e64b4ee
tsogbadrakh <tsogbadrakh@globalsign.com>
2022-02-06 15:58:26 +0900

    Finished to work on mystery pair list controller

--- spec/requests/admin/mystery_pairs_spec.rb
+++ spec/requests/admin/mystery_pairs_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Admin::MysteryPairs', type: :request do
   let!(:user) { create(:user, password: '!QAZ2wsx') }
+  let!(:mystery_pairs) { create_list(:mystery_pair, 10) }
 
   before do
     user.admin!
@@ -10,14 +11,14 @@ RSpec.describe 'Admin::MysteryPairs', type: :request do
 
   describe 'GET /index' do
     it 'returns http success' do
-      get '/admin/mystery_pairs/index'
+      get '/admin/mystery_pairs'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/admin/mystery_pairs/show'
+      get admin_mystery_pair_path(mystery_pairs.first)
       expect(response).to have_http_status(:success)
     end
   end
