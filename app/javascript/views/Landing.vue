9696d0fea68214be266b3374b3ef6756d81ef45f
tsogbadrakh <tsogbadrakh@globalsign.com>
2022-02-06 19:11:41 +0900

    Let me keep this idea of multiple pages

--- app/javascript/views/Landing.vue
+++ app/javascript/views/Landing.vue
@@ -1,24 +1,27 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
-      <v-flex xs12 class="text-xs-center" mt-5>
-        <h1>Welcome to Awesome App</h1>
-      </v-flex>
-      <v-flex xs12 sm6 offset-sm3 mt-3>
-        <blockquote class="blockquote text-xs-center">
-          It's a basic Single Page application <br>
-          with Google Firebase authentication
-        </blockquote>
-      </v-flex>
-      <v-flex xs12 sm6 offset-sm3 class="text-xs-center" mt-5>
-        <v-btn color="primary" to="/signup">Sign Up</v-btn>
-        <v-btn to="/signin">Sign In</v-btn>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <v-container fluid fill-height class="home-hero">
+        <v-layout justify-center align-center column pa-5>
+            <div class="display-4 font-weight-black white--text text-xs-center">MYSTERY LUNCH</div>
+            <div class="display-4 font-weight-black white--text text-xs-center mb-3">FOR YOU AND YOUR PARTNER</div>
+            <div class="display-1 font-weight-bold white--text text-xs-center">Simple Lunch will change your company culture.</div>
+            <v-btn fab class="mt-5 brown darken-4">
+            <v-icon large color="white">expand_more</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-container>
 </template>
+
 <script>
 export default {
-  //name: "Landing"
+    name: 'HomeHero'
 };
 </script>
+
+<style scoped>
+.home-hero {
+    background: url('http://source.unsplash.com/0BhSKStVtdM');
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
+</style>
